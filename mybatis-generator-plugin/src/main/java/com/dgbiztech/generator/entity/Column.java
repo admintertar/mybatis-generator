b52895c84940421c523490d473f1f9fd947a1ee8
钟鸿平 <hgcsjbf8>
2019-02-10 17:05:07 +0800

    添加display

--- mybatis-generator-plugin/src/main/java/com/dgbiztech/generator/entity/Column.java
+++ mybatis-generator-plugin/src/main/java/com/dgbiztech/generator/entity/Column.java
@@ -1,6 +1,7 @@
 package com.dgbiztech.generator.entity;
 
 import com.dgbiztech.generator.utils.StringUtils;
+import com.dgbiztech.generator.utils.Utils;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.config.Context;
@@ -10,6 +11,7 @@ public class Column {
     public final String fieldName;//该字段entity中的变量名称
     public final String fieldType;//该字段的类型
     public final String remarks;//该字段的注释
+    public final String display;//js显示的类型
 
     public Column(Context context, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Table table) {
         //如果没有注释，那么写字段名
@@ -23,6 +25,7 @@ public class Column {
         fieldName = introspectedColumn.getJavaProperty();
         fieldType = introspectedColumn.getFullyQualifiedJavaType().getFullyQualifiedName();
         actualName = introspectedColumn.getActualColumnName();
+        display = Utils.showDisplay(fieldType);
     }
 
     public String getActualName() {
@@ -35,6 +38,10 @@ public class Column {
 
     public String getFieldType() {
         return fieldType;
+    }
+
+    public String getDisplay() {
+        return display;
     }
 
     public String getRemarks() {
