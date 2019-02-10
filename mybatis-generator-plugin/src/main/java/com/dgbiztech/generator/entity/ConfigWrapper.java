338d2462bdd7527e2d815e341b0aaa3e141cc133
钟鸿平 <hgcsjbf8>
2019-02-10 16:18:48 +0800

    更新内部配置

--- mybatis-generator-plugin/src/main/java/com/dgbiztech/generator/entity/ConfigWrapper.java
+++ mybatis-generator-plugin/src/main/java/com/dgbiztech/generator/entity/ConfigWrapper.java
@@ -4,14 +4,8 @@ import com.dgbiztech.generator.config.TemplateConfig;
 
 import java.util.List;
 
-/**
- * Created By spawpaw@hotmail.com  2018-03-25
- *
- * @author BenBenShang spawpaw@hotmail.com
- */
 public class ConfigWrapper {
     private List<TemplateConfig> templateConfig;
-
 
     public List<TemplateConfig> getTemplateConfig() {
         return templateConfig;
