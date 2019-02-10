02c2a7f3664fd2de5e69e7f60ae8a6b800142447
钟鸿平 <hgcsjbf8>
2019-02-10 16:25:34 +0800

    精简jar

--- mybatis-generator-plugin/src/main/java/com/dgbiztech/generator/entity/Table.java
+++ mybatis-generator-plugin/src/main/java/com/dgbiztech/generator/entity/Table.java
@@ -3,8 +3,6 @@ package com.dgbiztech.generator.entity;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.config.Context;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
